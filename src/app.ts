245e862f48d2856e030840535ae8147c8fe8ad59
MostakemHossain <mostakememon123@gmail.com>
2024-08-25 19:35:00 +0600

    handle validation

--- src/app.ts
+++ src/app.ts
@@ -11,8 +11,7 @@ app.use(express.json());
 app.use(
   cors({
     origin: ["https://pet-adoption-front-end-sandy.vercel.app"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
   })
 );
