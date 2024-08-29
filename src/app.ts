683658874ff0d36d7daa5c3b1326972318b4023a
MostakemHossain <mostakememon123@gmail.com>
2024-08-29 23:13:18 +0600

    cors error

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,14 @@
 import cookieParser from "cookie-parser";
-import cors from "cors";
 import express, { Application, Request, Response } from "express";
 import globalErrorHandler from "./app/middlewares/golbalErrorHandler";
 import notFound from "./app/middlewares/notFound";
+import cors from "cors"
 import router from "./app/routes";
 const app: Application = express();
 
 // parser
 app.use(express.json());
-app.use(
-  cors({
-    origin: [
-      "https://pet-adoption-front-end-sandy.vercel.app",
-      "http://localhost:3000",
-    ],
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
